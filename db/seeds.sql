d81726e24898b8f2dab240367dd455f32be03de2
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-01-08 14:08:12 -0800

    added burgers database, schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Spicy Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Triple Cheese Bacon burger");
