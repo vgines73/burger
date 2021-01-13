9d6dc30d4bb3a9d26d115bf881109fb1049fbba9
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-01-13 00:22:32 -0800

    added demo into readme.

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,16 @@
 // Set up MySQL connection
 const mysql = require('mysql');
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db',
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'hacktheplanet',
+//     database: 'todoagain_db',
+//   });
+// }
 
 const connection = mysql.createConnection({
   host: 'localhost',
